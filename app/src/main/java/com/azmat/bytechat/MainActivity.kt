8f7a02ff053caf2206b3def0db27a4a5138f1573
Azmat Alam <93484428+azmatroshan@users.noreply.github.com>
2023-08-21 05:00:57 +0530

    init

---
+++ app/src/main/java/com/azmat/bytechat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.azmat.bytechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azmat.bytechat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
